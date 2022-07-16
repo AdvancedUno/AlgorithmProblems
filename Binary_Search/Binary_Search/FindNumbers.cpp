605249f72a3f44b0cfb8bdf5102c7789ba124084
AdvancedUno <ehlee8276@gmail.com>
2022-07-17 01:02:11 +0900

    FindNumbers

--- Binary_Search/Binary_Search/FindNumbers.cpp
+++ Binary_Search/Binary_Search/FindNumbers.cpp
@@ -7,82 +7,50 @@ using namespace std;
 int vecStoreAll[1000000];
 int iWantedNum;
 bool bAnswer[1000000] = { 0, };
-
+int iNumCasesCnt;
+int iWantedNumCnt;
 
 
 bool Search_B(int iWantedNumber, int startIdx, int endIdx) {
-
-
 	int midIdx = 0;
-
 	if (endIdx < startIdx) {
 		return 0;
 	}
 
-
 	midIdx = (endIdx + startIdx) / 2;
-
 	if (vecStoreAll[midIdx] == iWantedNumber) {
 		return 1;
 	}
 
-
-
 	if (vecStoreAll[midIdx] > iWantedNumber) {
-
 		if (Search_B(iWantedNumber, startIdx, midIdx - 1)) {
 			return 1;
 		}
-
 	}
 	else {
 		if (Search_B(iWantedNumber, midIdx + 1, endIdx)) {
 			return 1;
 		}
-
 	}
-
-
-
-
-
 	return 0;
-
 }
 
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> iNumCasesCnt;
 
-
-	int numCases;
-	int wantedNum;
-
-
-	cin >> numCases;
-
-
-
-	for (int i = 0; i < numCases; i++) {
-
+	for (int i = 0; i < iNumCasesCnt; i++) {
 		cin >> vecStoreAll[i];
-
-
-
 	}
 
-	sort(vecStoreAll, vecStoreAll + numCases);
+	sort(vecStoreAll, vecStoreAll + iNumCasesCnt);
 
-	cin >> wantedNum;
+	cin >> iWantedNumCnt;
 
-
-	for (int i = 0; i < wantedNum; i++) {
+	for (int i = 0; i < iWantedNumCnt; i++) {
 		cin >> iWantedNum;
-		cout << Search_B(iWantedNum, 0, numCases - 1) << "\n";
+		cout << Search_B(iWantedNum, 0, iNumCasesCnt - 1) << "\n";
 	}
-
-
-
-
 }
