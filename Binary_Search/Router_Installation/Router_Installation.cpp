135174ab7d7eb35414b297268634704c4a8911bb
AdvancedUno <ehlee8276@gmail.com>
2022-07-20 22:22:47 +0900

    Router_install try 1 - outofbounds error

--- Binary_Search/Router_Installation/Router_Installation.cpp
+++ Binary_Search/Router_Installation/Router_Installation.cpp
@@ -1,20 +1,72 @@
-﻿// Router_Installation.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <math.h> 
+#include <algorithm>
+#include <vector>
 
-#include <iostream>
+using namespace std;
 
-int main()
+long long N, K;
+
+
+long long lineStoreArray[100001];
+
+
+
+long long Calculate(int n)
 {
-    std::cout << "Hello World!\n";
+	long long midKey, Left = 1, Right = lineStoreArray[N-1] - lineStoreArray[0];
+	long long cntNum = 1;
+	midKey = (Left + Right) / 2;
+	long long ans = 0;
+	long long currentNum;
+
+	while (Left <= Right){
+	
+
+		cntNum = 1;
+		currentNum = lineStoreArray[0];
+		for (long long point = 1; point < N; point++) {
+			long long temp = lineStoreArray[point] - currentNum;
+			if (temp >= midKey) {
+				cntNum ++;
+				currentNum = lineStoreArray[point];
+			}
+		}
+
+		if (cntNum >= K) {
+			//midKey = (lineStoreArray[N - 1] + midKey) / 2 + midKey;
+			Left = midKey + 1;
+			if (ans <= midKey) {
+				ans = midKey;
+			}
+		}
+
+		if (cntNum < K) {
+			//midKey = midKey / 2;
+			Right = midKey - 1;
+		}
+
+		midKey = (Right + Left) / 2;
+
+	}
+	return ans;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+int main(void)
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	cin >> N;
+	cin >> K;
+	for (long long i = 0; i < N; i++)
+		cin >> lineStoreArray[i];
+
+	sort(lineStoreArray, lineStoreArray + N);
+
+
+
+	cout << Calculate(K);
+
+	return 0;
+}
