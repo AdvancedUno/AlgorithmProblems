a56a07180c883381366e5901f3082f300258b9ed
AdvancedUno <ehlee8276@gmail.com>
2022-07-21 23:36:01 +0900

    add heap

--- Priority_Queue/Priority_Queue/Priority_Queue.cpp
+++ Priority_Queue/Priority_Queue/Priority_Queue.cpp
@@ -1,20 +1,86 @@
-﻿// Priority_Queue.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <math.h> 
+#include <algorithm>
+#include <vector>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+long long N, K;
+
+
+long long lineStoreArray[100001];
+
+void DeleteMaxHeap() {
+
+
+
+
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+void InsertMaxHeap(int n, int currentIdx)
+{
+	lineStoreArray[currentIdx] = n;
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+
+	for (int i = currentIdx; i > 1; i=i/2) {
+
+		if (lineStoreArray[i] > lineStoreArray[i / 2]) {
+			
+			int temp = lineStoreArray[i / 2];
+			//cout << lineStoreArray[i] << " " << temp << endl;
+			lineStoreArray[i / 2] = lineStoreArray[i];
+			lineStoreArray[i] = temp;
+		}
+		else {
+			cout << lineStoreArray[i] << " " << lineStoreArray[i / 2] << endl;
+			break;
+		}
+
+	}
+
+}
+
+
+
+
+
+int main(void)
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	//cin >> K;
+	int numberTemp;
+	int currentIdx = 1;
+	for (long long i = 0; i < N; i++) {
+		cin >> numberTemp;
+		if (numberTemp == 0) {
+			DeleteMaxHeap();
+			if (currentIdx > 1) {
+				currentIdx--;
+			}
+		}
+		else {
+			InsertMaxHeap(numberTemp, currentIdx);
+			currentIdx++;
+		}
+		
+
+	}
+
+
+	for (int i =1; i < N+1; i++) {
+		cout << lineStoreArray[i] << endl;
+	}
+		
+		
+
+	//sort(lineStoreArray, lineStoreArray + N);
+
+
+
+	//cout << Calculate(K);
+
+	return 0;
+}
