52bc7d3d6dcdf9c89f278f9206ccc5dd5129e613
AdvancedUno <ehlee8276@gmail.com>
2022-07-25 21:25:30 +0900

    DFS clear unused coda

--- Graph_Algorithm/Depth_First_Search_Basic/Depth_First_Search_Basic.cpp
+++ Graph_Algorithm/Depth_First_Search_Basic/Depth_First_Search_Basic.cpp
@@ -9,44 +9,29 @@
 
 using namespace std;
 
-
+int N, iNumConnection, iStartNode;
 vector<int> iStoreConnectionArray[100001];
 int bCheckVisitedArray[100001] = { 0,};
 int bResultArray[100001];
 int cnt = 0;
 
 void DFS(int iWantedNode) {
-
-
 	if (bCheckVisitedArray[iWantedNode] == 1) {
 		return;
 	}
-	
 	bCheckVisitedArray[iWantedNode] = 1;
 	cnt ++;
 	bResultArray[iWantedNode] = cnt;
-
-
 	for (int i = 0; i < iStoreConnectionArray[iWantedNode].size(); i++) {
-
 		DFS(iStoreConnectionArray[iWantedNode][i]);
-
 	}
-
-
 }
-
 
 
 int main(void)
 {
-	/*cin.tie(NULL);
-	ios::sync_with_stdio(false);*/
-
-	int N, iNumConnection, iStartNode;
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-
 	cin >> N;
 	cin >> iNumConnection;
 	cin >> iStartNode;
@@ -57,19 +42,14 @@ int main(void)
 		iStoreConnectionArray[tempB].push_back(tempA);
 		iStoreConnectionArray[tempA].push_back(tempB);
 	}
-
 	for (int i = 1; i <= N; i++) {
 		sort(iStoreConnectionArray[i].begin(), iStoreConnectionArray[i].end());
 	}
-
 	DFS(iStartNode);
 
 	for (int i = 1; i <= N; i++) {
 
 		printf("%d\n", bResultArray[i]);
 	}
-
-
-	//return 0;
 }
 
