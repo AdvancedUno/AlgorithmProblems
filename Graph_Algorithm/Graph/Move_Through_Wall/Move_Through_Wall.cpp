01409416b3e9870b5df6c745871f739e2389062f
AdvancedUno <ehlee8276@gmail.com>
2022-07-31 19:04:50 +0900

    delete unused code for move_throuth_wall

--- Graph_Algorithm/Graph/Move_Through_Wall/Move_Through_Wall.cpp
+++ Graph_Algorithm/Graph/Move_Through_Wall/Move_Through_Wall.cpp
@@ -1,39 +1,21 @@
 ﻿#include<iostream>
 #include<queue>
-#include<deque>
 #include<string.h>
 #include<math.h>
 #include<cmath>
-#include<stack>
 #include<algorithm>
 
 using namespace std;
 
-int N, Rows, iStartNodeFirst, iStartNodeSecond;
+int N, Rows;
 int iStoreConnectionArray[1001][1001];
 int bCheckVisitedArray[1001][1001][2];
-vector<pair<int, int>> bWantResultVector;
-int iCntWall = 0;
-int iCntEmptyBox = 0;
-
 int iDirectionListFirst[4] = { -1,0,1,0 };
 int iDirectionListSecond[4] = { 0,1,0,-1 };
-
-
-bool CheckError(int iFirst, int iSecond) {
-
-	if (iFirst > N || iFirst <= 0 || iSecond > Rows || iSecond <= 0) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
 
 bool is_inside(int _ny, int _nx) {
 	return (_ny >= 1 && _ny <= N && _nx >= 1 && _nx <= Rows);
 }
-
 
 int BFS() {
 
@@ -41,11 +23,7 @@ int BFS() {
 
 	qSavePathInfo.push({ { 1,1 }, 1 });
 	bCheckVisitedArray[1][1][1] = 1;
-
-
 	while (!qSavePathInfo.empty()) {
-		
-		
 		int iPosY = qSavePathInfo.front().first.first;
 		int iPosX = qSavePathInfo.front().first.second;
 		int iCheckWall = qSavePathInfo.front().second;
@@ -56,7 +34,6 @@ int BFS() {
 			return bCheckVisitedArray[iPosY][iPosX][iCheckWall];
 		}
 
-
 		for (int i = 0; i < 4; i++) {
 
 			int iHMove = iPosY + iDirectionListFirst[i];
@@ -64,19 +41,13 @@ int BFS() {
 			if (!is_inside(iHMove, iVMove)) continue;
 			if (iCheckWall == 1 && iStoreConnectionArray[iHMove][iVMove] == 1) {
 				bCheckVisitedArray[iHMove][iVMove][iCheckWall-1] = bCheckVisitedArray[iPosY][iPosX][iCheckWall] + 1;
-				//qSavePathInfo.push(make_pair(make_pair(iHMove, iVMove), iCheckWall -1));
-				
-				qSavePathInfo.push({ {iHMove, iVMove}, iCheckWall - 1 });
+				qSavePathInfo.push(make_pair(make_pair(iHMove, iVMove), iCheckWall -1));
 			}
 			else if (iStoreConnectionArray[iHMove][iVMove] == 0 && bCheckVisitedArray[iHMove][iVMove][iCheckWall] == 0) {
 				bCheckVisitedArray[iHMove][iVMove][iCheckWall] = bCheckVisitedArray[iPosY][iPosX][iCheckWall] + 1;
-				//qSavePathInfo.push(make_pair(make_pair(iHMove, iVMove), iCheckWall));
-				
-				qSavePathInfo.push({ {iHMove, iVMove}, iCheckWall });
+				qSavePathInfo.push(make_pair(make_pair(iHMove, iVMove), iCheckWall));
 			}
 		}
-
-
 	}
 	return -1;
 }
@@ -87,7 +58,6 @@ int main(void)
 	ios::sync_with_stdio(false);
 	cin >> N;
 	cin >> Rows;
-
 	
 	string str;
 
@@ -101,25 +71,6 @@ int main(void)
 			else iStoreConnectionArray[i][j+1] = 0;
 		}
 	}
-
-
-	for (int i = 1; i <= N; i++) {
-
-
-		for (int j = 0; j < Rows; j++) {
-			
-			//cout << iStoreConnectionArray[i][j+1] << " ";
-
-
-		}
-		//cout << "\n";
-	}
-
-	
-
-
-	//BFS();
-
 
 	printf("%d\n", BFS());
 }
