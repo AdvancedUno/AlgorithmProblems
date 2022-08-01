dd00967fda8c14d08f103dfcee1ef6ed07484e59
AdvancedUno <ehlee8276@gmail.com>
2022-08-02 00:34:31 +0900

    Finding_Shortest_Path

---
+++ Finding_Shortest_Path/Dijkstra_Algorithm/Dijkstra_Algorithm.cpp
@@ -0,0 +1,94 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<queue>
+#include<math.h>
+#include<stdio.h>
+#include<cmath>
+#include<algorithm>
+#include <queue>
+
+#define endl "\n"
+#define INF 987654321
+
+
+using namespace std;
+
+
+int N, iNumConnection, iStartNode;
+vector<pair<int,int>> iStoreConnectionArray[20010];
+int iParentInfoArray[20010];
+int iResultSaveArray[20010];
+
+
+
+
+
+void BFS(int iWantedNode) {
+	priority_queue<pair<int, int>> qSaveCount;
+	iResultSaveArray[iWantedNode] = 0;
+	qSaveCount.push({ 0, iWantedNode});
+
+
+	while (!qSaveCount.empty()) {
+
+		int iCurrentNode = qSaveCount.top().second;
+		int iCurrentWeight = -qSaveCount.top().first;
+		qSaveCount.pop();
+		if (iResultSaveArray[iCurrentNode] < iCurrentWeight) {
+			continue;
+		}
+
+		for (int i = 0; i < iStoreConnectionArray[iCurrentNode].size(); i++) {
+
+			int iNextNode = iStoreConnectionArray[iCurrentNode][i].first;
+			int iNextWeight = iStoreConnectionArray[iCurrentNode][i].second;
+
+			if (iResultSaveArray[iNextNode] > iNextWeight + iCurrentWeight) {
+				iResultSaveArray[iNextNode] = iNextWeight + iCurrentWeight;
+				qSaveCount.push(make_pair(-iResultSaveArray[iNextNode],iNextNode));
+			}
+		}
+
+
+
+	}
+}
+
+
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	cin >> iNumConnection;
+	cin >> iStartNode;
+
+	for (int i = 1; i <= iNumConnection; i ++) {
+		iResultSaveArray[i] = INF;
+	}
+
+	for (int i = 1; i <= iNumConnection; i++) {
+		int tempA, tempB, tempWeight;
+		cin >> tempA >> tempB >> tempWeight;
+		iStoreConnectionArray[tempA].push_back(make_pair(tempB, tempWeight));
+	}
+
+
+	BFS(iStartNode);
+
+	for (int i = 1; i <= N; i++) {
+		if (iResultSaveArray[i] == INF) {
+			cout << "INF" << endl;
+		}
+		else {
+			cout << iResultSaveArray[i] << endl;
+			
+		}
+		
+	}
+}
+
