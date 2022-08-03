e7a509d2edd52a786ee37763a214bf472d885682
AdvancedUno <ehlee8276@gmail.com>
2022-08-03 23:34:09 +0900

    Hide_and_Seek updata

--- Graph_Algorithm/Graph/Hide_and_Seek/Hide_and_Seek.cpp
+++ Graph_Algorithm/Graph/Hide_and_Seek/Hide_and_Seek.cpp
@@ -1,20 +1,85 @@
-﻿// Hide_and_Seek.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include<iostream>
+#include<queue>
+#include<math.h>
+#include<stdio.h>
+#include<cmath>
+#include<algorithm>
+#include <queue>
 
-#include <iostream>
+#define endl "\n"
+#define INF 987654321
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+
+int N, iNumConnection, iStartNode;
+vector<pair<int, int>> iStoreConnectionArray[20010];
+int iParentInfoArray[20005];
+int iResultSaveArray[20005];
+
+
+
+
+
+void BFS(int iWantedNode) {
+	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > qSaveCount;
+	iResultSaveArray[iWantedNode] = 0;
+	qSaveCount.push({ 0, iWantedNode });
+
+
+	while (!qSaveCount.empty()) {
+
+		int iCurrentNode = qSaveCount.top().second;
+		int iCurrentWeight = qSaveCount.top().first;
+		qSaveCount.pop();
+		if (iResultSaveArray[iCurrentNode] < iCurrentWeight) {
+			continue;
+		}
+
+		for (int i = 0; i < iStoreConnectionArray[iCurrentNode].size(); i++) {
+
+			int iNextNode = iStoreConnectionArray[iCurrentNode][i].first;
+			int iNextWeight = iStoreConnectionArray[iCurrentNode][i].second;
+
+			if (iResultSaveArray[iNextNode] > iNextWeight + iCurrentWeight) {
+				iResultSaveArray[iNextNode] = iNextWeight + iCurrentWeight;
+				qSaveCount.push(make_pair(iResultSaveArray[iNextNode], iNextNode));
+			}
+		}
+
+	}
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	cin >> iNumConnection;
+	cin >> iStartNode;
+
+	for (int i = 1; i <= iNumConnection; i++) {
+		int tempA, tempB, tempWeight;
+		cin >> tempA >> tempB >> tempWeight;
+		iStoreConnectionArray[tempA].push_back(make_pair(tempB, tempWeight));
+	}
+	for (int i = 1; i <= N; i++) {
+		iResultSaveArray[i] = INF;
+	}
+
+	BFS(iStartNode);
+
+	for (int i = 1; i <= N; i++) {
+		if (iResultSaveArray[i] == INF) {
+			cout << "INF" << endl;
+		}
+		else {
+			cout << iResultSaveArray[i] << endl;
+		}
+	}
+}
