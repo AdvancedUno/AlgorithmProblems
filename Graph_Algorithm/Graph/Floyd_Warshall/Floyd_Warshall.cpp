fb108fa4b42696c5363c744f52f55465a7993bce
AdvancedUno <ehlee8276@gmail.com>
2022-08-06 19:25:48 +0900

    Floyd Warshall first try

--- Graph_Algorithm/Graph/Floyd_Warshall/Floyd_Warshall.cpp
+++ Graph_Algorithm/Graph/Floyd_Warshall/Floyd_Warshall.cpp
@@ -1,1 +1,97 @@
-﻿
+﻿#include<iostream>
+#include<math.h>
+#include<stdio.h>
+#include<algorithm>
+#include <queue>
+
+#define MAX_SIZE 500+1
+#define endl "\n"
+#define INF 987654321 
+
+
+using namespace std;
+
+
+vector<pair<int, int>> iStoreConnectionArray[MAX_SIZE];
+
+int iNodeNum, iConnectionNum, iStartPos;
+
+int iResultSaveArray[MAX_SIZE][MAX_SIZE];
+
+
+bool BellmanFord(int iWantedNode) {
+	
+	for (int loop = 1; loop <= iNodeNum; loop++) {
+		iResultSaveArray[loop][loop] = 0;
+		for (int i = 1; i <= iNodeNum; i++) {
+			for (int j = 0; j < iStoreConnectionArray[i].size(); j++) {
+				int iCurrentNode = i;
+				int iNextNode = iStoreConnectionArray[iCurrentNode][j].first;
+				int iNextWeight = iStoreConnectionArray[iCurrentNode][j].second;
+
+				//if (iResultSaveArray[iCurrentNode] == INF)continue;
+
+				if (iResultSaveArray[iCurrentNode][iNextNode] > iNextWeight + iResultSaveArray[iCurrentNode][iNextNode-1]) {
+					iResultSaveArray[iCurrentNode][iNextNode] = iNextWeight + iResultSaveArray[iCurrentNode][iNextNode - 1];
+				}
+
+			}
+
+		}
+	}
+
+	for (int i = 1; i <= iNodeNum; i++) {
+		for (int j = 1; j <= iNodeNum; j++) {
+			if (iResultSaveArray[i][j] != INF) {
+				cout << iResultSaveArray[i][j] << " ";
+			}
+			else {
+				cout << 0 << " ";
+			}
+		}
+
+		cout << "\n";
+	}
+
+
+	return false;
+}
+
+void Reset() {
+	for (int i = 1; i <= iNodeNum; i++) {
+		for (int j = 1; j <= iNodeNum; j++) {
+			iResultSaveArray[i][j] = INF;
+		}
+	}
+
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	cin >> iNodeNum >> iConnectionNum;
+
+	Reset();
+	for (int j = 0; j < iConnectionNum; j++) {
+
+		int tempA, tempB, tempWeight;
+		cin >> tempA >> tempB >> tempWeight;
+
+		iResultSaveArray[tempA][tempB] = tempWeight;
+		iStoreConnectionArray[tempA].push_back(make_pair(tempB, tempWeight));
+	}
+
+
+	iStartPos = 1;
+	
+	BellmanFord(iStartPos);
+
+
+
+	return 0;
+
+}
