2f5dc4e16f4b9fd1f7ddb4ee3d4cca72e445ac8f
AdvancedUno <ehlee8276@gmail.com>
2022-08-08 22:19:10 +0900

    Second try

--- Backtrack_Shortest_Path/Increase_Subsequence/Increase_Subsequence.cpp
+++ Backtrack_Shortest_Path/Increase_Subsequence/Increase_Subsequence.cpp
@@ -5,39 +5,43 @@
 
 using namespace std;
 int iNum;
-vector<int> iSaveVec;
+int iSaveVec[1001];
 int iSavePast[1001];
+int iSavePrev[1001];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 	cin >> iNum;
 
-	for (int i = 0; i < iNum; i++) {
+	for (int i = 1; i < iNum; i++) {
 		int iTemp;
 		cin >> iTemp;
-		iSaveVec.push_back(iTemp);
+		iSaveVec[i] = iTemp;
 	}
 
 
 	int iCnt = 0;
 	iSavePast[0] = 1;
-	for (int i = 1; i < iNum; i++) {
+	for (int i = 1; i <= iNum; i++) {
 		for (int j = i - 1; j >= 0; j--) {
 			if (iSaveVec[i] > iSaveVec[j]) {
 				iSavePast[i] = max(iSavePast[i], iSavePast[j] +1);
 			}
+			if (iSavePast[j] +1 < iSavePast[i]) {
+				iSavePrev[i] = iSaveVec[i];
+			}
 		}
+
 		iCnt = max(iCnt, iSavePast[i]);
 	}
 	cout << iCnt << endl;
 	
-
-
-
-
-
-
+	for (int i = 0; i < iNum; i++) {
+		if (iSavePrev[i] != 0) {
+			cout << iSavePrev[i] << " ";
+		}
+	}
 
 
 	
