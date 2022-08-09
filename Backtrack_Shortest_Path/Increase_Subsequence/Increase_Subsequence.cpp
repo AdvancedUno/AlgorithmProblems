24b29ff633418dba66662d0f8bb7daeaead6fba8
AdvancedUno <108073642+AdvancedUno@users.noreply.github.com>
2022-08-09 09:45:06 +0900

    Update Increase_Subsequence.cpp

--- Backtrack_Shortest_Path/Increase_Subsequence/Increase_Subsequence.cpp
+++ Backtrack_Shortest_Path/Increase_Subsequence/Increase_Subsequence.cpp
@@ -11,10 +11,10 @@ int iSavePrev[1001];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cout.tie(0);
+	std::cout.tie(0);
 	cin >> iNum;
 
-	for (int i = 1; i < iNum; i++) {
+	for (int i = 1; i <= iNum; i++) {
 		int iTemp;
 		cin >> iTemp;
 		iSaveVec[i] = iTemp;
@@ -22,28 +22,37 @@ int main() {
 
 
 	int iCnt = 0;
-	iSavePast[0] = 1;
-	for (int i = 1; i <= iNum; i++) {
+	int iMaxPos = 0;
+	int iTrackLarge = 0;
+
+
+	for (int i = 0; i <= iNum; i++) {
 		for (int j = i - 1; j >= 0; j--) {
 			if (iSaveVec[i] > iSaveVec[j]) {
-				iSavePast[i] = max(iSavePast[i], iSavePast[j] +1);
+				if (iSavePast[i] < iSavePast[j] + 1) {
+					iSavePrev[i] = j;
+				}
+				iSavePast[i] = max(iSavePast[i], iSavePast[j] + 1);
 			}
-			if (iSavePast[j] +1 < iSavePast[i]) {
-				iSavePrev[i] = iSaveVec[i];
-			}
-		}
 
+		}
+		if (iSavePast[i] > iCnt) {
+			iMaxPos = i;
+		}
 		iCnt = max(iCnt, iSavePast[i]);
 	}
-	cout << iCnt << endl;
-	
-	for (int i = 0; i < iNum; i++) {
-		if (iSavePrev[i] != 0) {
-			cout << iSavePrev[i] << " ";
-		}
+	std::cout << iCnt << endl;
+
+	vector<int> iTemp;
+	while (iMaxPos > 0) {
+		//cout << iSaveVec[iMaxPos] << " ";
+		iTemp.push_back(iSaveVec[iMaxPos]);
+		iMaxPos = iSavePrev[iMaxPos];
 	}
 
+	for(int i = iTemp.size() - 1; i >= 0; i--) {
+		cout << iTemp[i] << " ";
+	}
 
-	
 	return 0;
 }
