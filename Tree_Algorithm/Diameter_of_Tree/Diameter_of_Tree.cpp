3e6658524049a3e0d3099a53883d52053ab3e19e
AdvancedUno <ehlee8276@gmail.com>
2022-08-14 20:11:36 +0900

    Diameter of Tree start

--- Tree_Algorithm/Diameter_of_Tree/Diameter_of_Tree.cpp
+++ Tree_Algorithm/Diameter_of_Tree/Diameter_of_Tree.cpp
@@ -1,20 +1,49 @@
-﻿// Diameter_of_Tree.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿//
 
 #include <iostream>
+#include <iostream>
+#include<vector>
+#include <queue>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+vector<pair<int, int>> childTree[100001];
+int visited[100001];
+int parent[100001];
+int N;
+
+
+void DFS() {
+
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	std::cout.tie(0);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int temp1;
+		int temp2;
+		int iWeight;
+		cin >> temp1;
+		while (true) {
+			cin >> temp2;
+			if (temp2 == -1)break;
+			cin >> iWeight;
+			childTree[temp1].push_back(make_pair(temp2, iWeight));
+		}
+	}
+
+
+
+
+	for (int i = 1; i <= N; i++) {
+		cout << childTree[i].front().first << "\n";
+	}
+
+}
